cbbe0a5866b34ab3575f4acd3007a75ff183db11
nguyensonhai <nguyensonhai1009@gmail.com>
2020-03-29 22:11:22 +0700

    update

---
+++ Tuan_4/Bai_3/Bai_3/mainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_3
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "(*.txt)|*.txt|(All)|*.*";
+            saveDlg.FileName = txtFileName.Text;
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = saveDlg.OpenFile();//Open to Write
+                StreamWriter writer = new StreamWriter(stream);
+                writer.WriteLine(txtText.Text);
+                writer.Close();
+            }
+        }
+    }
+}
